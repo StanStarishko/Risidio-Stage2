4c055e59ffa598841962ba430c896ff6743e3a48
Stanislav Starishko <slavkost73@gmail.com>
2025-08-27 21:48:48 +0100

    Fix Deploy

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9592a4708bfd5df4"; }
+/******/ 		__webpack_require__.h = function() { return "e725c5fc254ac544"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
