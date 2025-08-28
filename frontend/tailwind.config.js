b9bb171f939478d5110d43e8a9d46eda2c047ea4
Stanislav Starishko <slavkost73@gmail.com>
2025-08-28 03:32:13 +0100

    Upd tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+// human
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.css",
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
